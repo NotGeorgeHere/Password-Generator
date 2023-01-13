f84051cfeafbc2f8230b5c9d08e375e3c1a82c03
unknown <ryder.george20@gmail.com>
2023-01-13 19:55:45 +0000

    sorted password requirements to return an array of requirements

--- script.js
+++ script.js
@@ -91,6 +91,26 @@ var upperCasedCharacters = [
 // Function to prompt user for password options
 function getPasswordOptions() {
 
+  //Generates the length of password and makes sure its between 8 and 128
+  var passRequirements = [];
+  var passLength = parseInt(prompt("Length of password?"));
+  if (passLength < 8 || passLength > 128 || isNaN(passLength) === true){
+    alert("Password Length has to be between 8 and 128 characters");
+    return;
+  }
+  //Generates confirmations asking if they want certain requirements, generates true/false
+  var lowerCase = confirm("Do you want lower case?");
+  var upperCase = confirm("Do you want upper case?");
+  var number = confirm("Do you want numbers?");
+  var specialChar = confirm("Do you want special characters?");
+  //Pushes password length into array to be searched through later using for loop to see
+  passRequirements.push(passLength);
+  passRequirements.push(lowerCase);
+  passRequirements.push(upperCase);
+  passRequirements.push(number);
+  passRequirements.push(specialChar);
+
+  return passRequirements;
 }
 
 // Function for getting a random element from an array
@@ -100,7 +120,8 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-
+  var passConditions = getPasswordOptions();
+  console.log(passConditions);
 }
 
 // Get references to the #generate element
